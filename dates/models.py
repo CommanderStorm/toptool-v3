4bcbc8bfe1df329d628454c6955e633d04431f51
Julian Biendarra <julian.biendarra@tum.de>
2015-12-09 11:35:56 +0100

    create models

--- dates/models.py
+++ dates/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Semester(models.Model):
+    wintersemester = models.BooleanField()
+
+    year = models.IntField()
+
+    def __str__(self):
+        if self.wintersemester:
+            return "Wintersemester {0: 4}/{1: 2}".format(self.year,
+                    self.year+1)
+        else:
+            return "Sommersemester {0: 4}".format(self.year)
+
+
+class Ausschuss(models.Model):
+    date = models.DateTimeField()
+
+    room = models.CharField(
+        max_length = 200,
+    )
+
+    semester = models.ForeignKey(
+        Semester,
+    )
+
+    def __str__(self):
+        return "Fachschaftsausschuss am {0} in {1}".format(self.date,
+                self.room)
+
+
--- tops/models.py
+++ tops/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from dates.models import Ausschuss
 
-# Create your models here.
+class Top(models.Model):
+    title = models.CharField(
+        max_length=200,
+    )
+
+    author = models.CharField(
+        max_length=50,
+    )
+
+    email = models.CharField(
+        max_length=100,
+    )
+
+    text = models.TextField()
+
+    ausschuss = models.ForeignKey(
+        Ausschuss,
+    )
+
+    date = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return "{0} ({1}, {2})".format(self.title, self.author, self.email)
+
+
