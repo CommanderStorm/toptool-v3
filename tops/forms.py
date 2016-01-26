0dd9269120e71a0219ce59ed5a0852d1943bc281
Julian Biendarra <julian.biendarra@tum.de>
2016-01-26 22:14:24 +0100

    delete tops

--- tops/forms.py
+++ tops/forms.py
@@ -13,7 +13,7 @@ class AddForm(forms.ModelForm):
         super(AddForm, self).__init__(*args, **kwargs)
 
     def save(self, commit=True):
-        instance = super(DelForm, self).save(False)
+        instance = super(AddForm, self).save(False)
 
         instance.meeting = self.meeting
 
@@ -30,14 +30,12 @@ class AddForm(forms.ModelForm):
 
 
 class DelForm(forms.Form):
+
     def __init__(self, *args, **kwargs):
-        self.meeting = kwargs.pop('meeting')
+        tops = kwargs.pop('tops')
 
         super(DelForm, self).__init__(*args, **kwargs)
         
-        tops = self.meeting.top_set.order_by('topid')
-        self.top = forms.ChoiceField(choices=[(top, "TOP {0}: {1}".format(
-            top.topid, top)) for top in tops])
-
-
+        self.fields['top'] = forms.ChoiceField(choices=[(top.topid,
+            "TOP {0}: {1}".format(top.topid, top)) for top in tops])
 
--- tops/views.py
+++ tops/views.py
@@ -17,13 +17,15 @@ def view(request, meeting_pk):
 
 def delete(request, meeting_pk):
     meeting = get_object_or_404(Meeting, pk=meeting_pk)
+    tops = meeting.top_set.order_by('topid')
 
     # response form
     # the "request.POST or None" shortens the view. otherwise one would have
     # to check if request.POST is not none and initializie the form then
-    form = DelForm(request.POST or None, meeting=meeting)
+    form = DelForm(request.POST or None, tops=tops)
     if form.is_valid():
-        form.save()
+        del_top = form.cleaned_data['top']
+        meeting.top_set.filter(topid=del_top).delete()
 
         return HttpResponseRedirect(reverse('view', args=[meeting.id]))
 
