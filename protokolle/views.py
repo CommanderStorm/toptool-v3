dfa30c7041dff434b3a5e8908a6bcd77f3df379b
Julian Biendarra <biendarra@fs.tum.de>
2016-02-29 20:58:23 +0100

    add protokoll template and view

--- protokolle/views.py
+++ protokolle/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import get_template
 
-# Create your views here.
+def protokoll_template(request, meeting_pk):
+    meeting = get_object_or_404(Meeting, pk=meeting_pk)
+    tops = meeting.top_set.order_by('topid')
+
+    # Create the HttpResponse object with the appropriate t2t header.
+    response = HttpResponse(content_type='text/t2t')
+
+    # TODO: add date etc. to default name?
+    response['Content-Disposition'] = 'attachment; filename=protokoll.t2t"'
+
+    text_template = get_template('protokolle/vorlage.t2t')
+    context = {
+        'meeting': meeting,
+        'tops': tops,
+    }
+
+    response.write(text_template.render(context))
+    return response
+
