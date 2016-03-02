ff6863b78241c723c43a1071c0d12579bcb415a6
Julian Biendarra <biendarra@fs.tum.de>
2016-03-02 01:01:26 +0100

    adjust protokoll template

--- protokolle/views.py
+++ protokolle/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template.loader import get_template
+
+from meetings.models import Meeting
 
 def template(request, meeting_pk):
     meeting = get_object_or_404(Meeting, pk=meeting_pk)
@@ -10,7 +12,7 @@ def template(request, meeting_pk):
     response = HttpResponse(content_type='text/t2t')
 
     # TODO: add date etc. to default name?
-    response['Content-Disposition'] = 'attachment; filename=protokoll.t2t"'
+    response['Content-Disposition'] = 'attachment; filename=protokoll.t2t'
 
     text_template = get_template('protokolle/vorlage.t2t')
     context = {
--- tops/models.py
+++ tops/models.py
@@ -37,6 +37,8 @@ class StandardTop(models.Model):
 
     description = models.TextField()
 
+    protokoll_template = models.TextField()
+
     meetingtype = models.ForeignKey(
         MeetingType,
     )
