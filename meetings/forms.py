1219cbac65c93344e305a2e7c9eb65bb299ff3cb
Julian Biendarra <biendarra@fs.tum.de>
2016-03-06 00:50:24 +0100

    add datetimepicker

--- meetings/forms.py
+++ meetings/forms.py
@@ -1,3 +1,4 @@
+from bootstrap3_datetime.widgets import DateTimePicker
 from django import forms
 from django.contrib.auth.models import User
 from django.db.models import Q
@@ -9,6 +10,15 @@ class MeetingForm(forms.ModelForm):
     class Meta:
         model = Meeting
         exclude = ['meetingtype', 'attendees']
+        widgets = {
+            'time': DateTimePicker(options={
+                "format": "DD.MM.YYYY HH:mm",
+                "minuteStepping": 5}),
+            'topdeadline': DateTimePicker(options={
+                "format": "DD.MM.YYYY HH:mm",
+                "minuteStepping": 5}),
+            }
+
 
     def __init__(self, *args, **kwargs):
         self.meetingtype = kwargs.pop('meetingtype')
@@ -20,6 +30,8 @@ class MeetingForm(forms.ModelForm):
             Q(groups__permissions=self.meetingtype.get_permission()))
         self.fields['sitzungsleitung'].queryset = users
         self.fields['protokollant'].queryset = users
+        self.fields['time'].input_formats=['%d.%m.%Y %H:%M']
+        self.fields['topdeadline'].input_formats=['%d.%m.%Y %H:%M']
 
 
     def save(self, commit=True):
@@ -35,15 +47,20 @@ class MeetingForm(forms.ModelForm):
 
 class MeetingSeriesForm(forms.Form):
     start = forms.DateTimeField(
-        _("Start"),
+        input_formats=['%d.%m.%Y %H:%M'],
+        widget=DateTimePicker(options={"format": "DD.MM.YYYY HH:mm",
+                                       "minuteStepping": 5}),
+        label=_("Start"),
     )
 
     end = forms.DateTimeField(
-        _("Ende"),
+        input_formats=['%d.%m.%Y %H:%M'],
+        widget=DateTimePicker(options={"format": "DD.MM.YYYY HH:mm",
+                                       "minuteStepping": 5}),
+        label=_("Ende"),
     )
 
     cycle = forms.ChoiceField(
-        _("Häufigkeit"),
         (
             (1, 'täglich'),
             (2, 'alle 2 Tage'),
@@ -52,6 +69,7 @@ class MeetingSeriesForm(forms.Form):
             (21, 'alle 3 Wochen'),
             (28, 'alle 4 Wochen'),
         ),
+        label = _("Häufigkeit"),
     )
 
     room = forms.CharField(
--- toptool/settings.py
+++ toptool/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'bootstrap3_datetime',
     'toptool_common',
     'meetingtypes',
     'meetings',
