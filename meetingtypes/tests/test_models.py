b0e3ddb09f21082ede22789813e62f56b9b6f1ba
Julian Biendarra <>
2017-10-23 16:33:11 +0200

    add test for meetingtypes

---
+++ meetingtypes/tests/test_models.py
@@ -0,0 +1,9 @@
+import pytest
+from mixer.backend.django import mixer
+
+pytestmark = pytest.mark.django_db
+
+class TestMeetingType:
+    def test_init(self):
+        obj = mixer.blend('meetingtypes.MeetingType')
+        assert obj.pk not in (None, ""), 'Should create a MeetingType instance'
