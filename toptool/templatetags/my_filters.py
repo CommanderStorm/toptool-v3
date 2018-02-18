0dd2a6f9142b7d781506bb7ed3be4cb8e873d1bf
Julian Biendarra <>
2018-02-18 12:42:28 +0100

    fix enumeration in agenda email and minutes template

--- toptool/templatetags/my_filters.py
+++ toptool/templatetags/my_filters.py
@@ -12,3 +12,13 @@ register = template.Library()
 def strip_empty_lines(value):
     nonempty_lines = [l for l in value.splitlines() if l.strip()]
     return "\n".join(nonempty_lines)
+
+
+@register.filter
+@stringfilter
+def strip_li(value):
+    regex = re.compile(r"^\s*<li>(.*)</li>")
+    lines = []
+    for line in value.splitlines():
+        lines.append(regex.sub(r"  - \1", line))
+    return "\n".join(lines)
