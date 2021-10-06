bcfaca362a5156ef8842b047322959036ccceef1
Frank Elsinga <frank@elsinga.de>
2021-10-07 00:25:58 +0200

    Moved from django.conf.url's to django.urls.path's Rason: url will be deprecated in Django 4.0 (Real reason: I cant stand how ugly they look)

--- persons/urls.py
+++ persons/urls.py
@@ -1,27 +1,18 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<meeting_pk>[0-9a-f\-]+)/addatt/$', views.add_attendees,
-        name="addattendees"),
-    url(r'^(?P<meeting_pk>[0-9a-f\-]+)/editatt/(?P<attendee_pk>[0-9]+)/$',
-        views.edit_attendee, name="editattendee"),
-    url(r'^(?P<meeting_pk>[0-9a-f\-]+)/delatt/(?P<attendee_pk>[0-9]+)/$',
-        views.delete_attendee, name="delattendee"),
-    url(r'^functions/$', views.functions, name="functions"),
-    url(r'^functions/sort/$', views.sort_functions, name="sortfunctions"),
-    url(r'^functions/(?P<function_pk>[0-9]+)/edit/$', views.edit_function,
-        name="editfunction"),
-    url(r'^functions/(?P<function_pk>[0-9]+)/delete/$', views.delete_function,
-        name="delfunction"),
-    url(r'^(?P<meeting_pk>[0-9a-f\-]+)/addperson/$', views.add_person,
-        name="addperson"),
-    url(r'^persons/$', views.persons, name="persons"),
-    url(r'^persons/add/$', views.add_plain_person,
-        name="addplainperson"),
-    url(r'^persons/(?P<person_pk>[0-9]+)/edit/$', views.edit_person,
-        name="editperson"),
-    url(r'^persons/(?P<person_pk>[0-9]+)/delete/$', views.delete_person,
-        name="delperson"),
+    path('<uuid:meeting_pk>/addatt/', views.add_attendees,        name="addattendees"),
+    path('<uuid:meeting_pk>/editatt/<int:attendee_pk>/',views.edit_attendee, name="editattendee"),
+    path('<uuid:meeting_pk>/delatt/<int:attendee_pk>/',views.delete_attendee, name="delattendee"),
+    path('functions/', views.functions, name="functions"),
+    path('functions/sort/', views.sort_functions, name="sortfunctions"),
+    path('functions/<int:function_pk>/edit/', views.edit_function,name="editfunction"),
+    path('functions/<int:function_pk>/delete/', views.delete_function,name="delfunction"),
+    path('<uuid:meeting_pk>/addperson/', views.add_person,name="addperson"),
+    path('persons/', views.persons, name="persons"),
+    path('persons/add/', views.add_plain_person,name="addplainperson"),
+    path('persons/<int:person_pk>/edit/', views.edit_person,name="editperson"),
+    path('persons/<int:person_pk>/delete/', views.delete_person,name="delperson"),
 ]
